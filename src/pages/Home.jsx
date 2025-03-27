d5906c82110cd21d074219752378bec4c7e3d178
Sravan Kumar <sravanpatel0017@gmail.com>
2025-03-27 23:29:05 +0530

    changed effect on image on the home file

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,10 +5,10 @@ export default function Home() {
     return (
         <div className='home-section' id="home">
             <motion.div
-                initial={{ x: -50, opacity: 0 }} // Start from left and transparent
-                animate={{ x: 0, opacity: 1 }} // Move to original position and become visible
+                initial={{ x: -50, opacity: 0 }} 
+                animate={{ x: 0, opacity: 1 }} 
                 transition={{
-                    type: "spring", // You can try "tween" or "spring"
+                    type: "spring",
                     stiffness: 100,
                     damping: 20,
                     duration: 0.5,
@@ -17,9 +17,18 @@ export default function Home() {
                 <p className='profile-summary'>I'm a passionate MERN stack developer with expertise in building dynamic and responsive web applications using MongoDB, Express.js, React.js, and Node.js. I’m also familiar with state management using hooks, responsive design with CSS frameworks like Tailwind, and improving performance with modern development tools.</p>
                 <button className='hireme-btn'>Contact Me</button>
             </motion.div>
-            <div className='profile-image-section'>
+            <motion.div
+                initial={{ x: 50, opacity: 0 }}
+                animate={{ x: 0, opacity: 1 }}
+                style={{ mixBlendMode: "darken" }}
+                transition={{
+                    type: "spring", 
+                    stiffness: 100,
+                    damping: 20,
+                    duration: 0.5,
+                }} className='profile-image-section'>
                 <img src={"../assets/portfolio_person-Photoroom.jpg"} alt="profile image" className='profile-image' />
-            </div>
+            </motion.div>
         </div>
     )
 }
